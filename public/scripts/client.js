f3e282cc0cd900863ca79f57ba67731cdb07d13f
diogenesoftoronto <diogenesoft@protonmail.com>
2021-12-22 21:29:35 -0500

    Some bugs present. dasktop layout acheived.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,8 +1,8 @@
 /*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+* Client-side JS logic goes here
+* jQuery is already loaded
+* Reminder: Use (and do all your DOM work in) jQuery's document ready function
+*/
 const esc = function (str) {
   let para = document.createElement("p");
   para.appendChild(document.createTextNode(str));
@@ -14,34 +14,34 @@ const createTweetElement = (obj) => {
     const tweetArticle =  
     `<article class="tweet">
     <div class='tweet-header'>
-      <div class='tweet-header-left'>
-        <img class='profile' src=${obj.user.avatars}></img>
-        <h3>${obj.user.name}</h3>
-      </div>
-      <div class='tweet-header-right'>
-        <h4>${obj.user.handle}</h4>
-      </div>
+    <div class='tweet-header-left'>
+    <img class='profile' src=${obj.user.avatars}></img>
+    <h3>${obj.user.name}</h3>
+    </div>
+    <div class='tweet-header-right'>
+    <h4>${obj.user.handle}</h4>
+    </div>
     </div>
     <div class='tweet-body'>
-      <p>${esc(obj.content.text)}</p>
+    <p>${esc(obj.content.text)}</p>
     </div>
     <div class='tweet-footer'>
-      <div class='tweet-footer-left'>
-        <h4>${timeago.format(obj.created_at)}</h4>
-      </div>
-      <div class='tweet-footer-right'>
-        <div>
-        <a class='tweet-icon' href='/'><i class="fas fa-flag"></i></a>
-        <a class='tweet-icon' href='/'><i class="fas fa-retweet"></i></a>
-        <a class='tweet-icon' href='/'><i class="fas fa-heart"></i></a>
-      </div>
+    <div class='tweet-footer-left'>
+    <h4>${timeago.format(obj.created_at)}</h4>
+    </div>
+    <div class='tweet-footer-right'>
+    <div>
+    <a class='tweet-icon' href='/'><i class="fas fa-flag"></i></a>
+    <a class='tweet-icon' href='/'><i class="fas fa-retweet"></i></a>
+    <a class='tweet-icon' href='/'><i class="fas fa-heart"></i></a>
+    </div>
     </div>
     </div>
     </article>`;
     $('section.tweet-container').prepend(tweetArticle);
     tweetInteract();
   })
-
+  
 }
 
 const renderTweets = (tweets) => {
@@ -49,44 +49,6 @@ const renderTweets = (tweets) => {
     createTweetElement(tweet)
   }
 }
-
-
-const tweetForm = () => {
-  $(document).ready(function() {
-    $('#post-tweet').submit(function (event) {
-      event.preventDefault();
-      const alertShort = 
-      `<div class="alert">
-        <i class="fas fa-exclamation-circle"></i>
-        <span class="alert">Tweets must be filled with your glory!</span>
-      </div>`
-      const alertLong = 
-      `<div class="alert">
-        <i class="fas fa-exclamation-circle"></i>
-        <span class="alert">Too Long Did Not Read!</span>
-      </div>`
-
-      const tweetBody = $('#tweet-text').serialize();
-      if (tweetBody.length < 6) {
-        // $().html("<label>Tweets must be filled with your glory!</label>").slideDown().slideUp();
-        $('section.new-tweet>h2').prepend(alertShort).slideDown();
-        shadowInteract()
-      } else if (tweetBody.length > 145) {
-        $('section.new-tweet>h2').prepend(alertLong);
-      } else {
-        if ($("div.alert").val() !== null || $("div.alert").val() !== undefined) {
-          // if the warning is visible slideUp and remove the element.
-          $("div.alert").slideUp().remove();
-
-        }
-        const form = $(this)
-        const url = form.attr('action')
-        $.post(url, tweetBody)
-      }
-    })
-  })
-};
-tweetForm();
 
 const loadTweets = () => {
   $(document).ready(function() {
@@ -113,4 +75,45 @@ const refetchTweets = () => {
     })
   })
 }
-refetchTweets()
+
+const tweetForm = () => {
+  $(document).ready(function() {
+    $('#post-tweet').submit(function (event) {
+      event.preventDefault();
+      const alertShort = 
+      `<div class="alert">
+        <i class="fas fa-exclamation-circle"></i>
+        <span class="alert">Tweets must be filled with your glory!</span>
+      </div>`
+      const alertLong = 
+      `<div class="alert">
+        <i class="fas fa-exclamation-circle"></i>
+        <span class="alert">Too Long Did Not Read!</span>
+      </div>`
+
+      const tweetBody = $('#tweet-text').serialize();
+      if (tweetBody.length < 6 ) {
+        // $().html("<label>Tweets must be filled with your glory!</label>").slideDown().slideUp();
+        console.log($('section.new-tweet>h2').val())
+        $('section.new-tweet>h2').prepend(alertShort).slideDown();
+        shadowInteract()
+        
+      } else if (tweetBody.length > 145) {
+        $('section.new-tweet>h2').prepend(alertLong);
+        shadowInteract()
+      } else {
+        if ($("div.alert").val() !== null || $("div.alert").val() !== undefined) {
+          // if the warning is visible slideUp and remove the element.
+          $("div.alert").slideUp().remove();
+
+        }
+        const form = $(this)
+        const url = form.attr('action')
+        $.post(url, tweetBody)
+        refetchTweets()
+      }
+    })
+  })
+};
+tweetForm();
+
